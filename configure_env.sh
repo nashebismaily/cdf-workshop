682bd80d5f630370ae7dd826ea4d72684838153f
root <root@ec2-3-234-251-241.compute-1.amazonaws.com>
2020-03-03 01:12:57 +0000

    test 1

--- configure_env.sh
+++ configure_env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -x
+
 HOSTNAME=$(hostname -f)
+CURRENT_DIR=$(pwd)
 
 # Install Dependencies
 yum -y install mysql-connector-java* unzip zip
@@ -11,13 +14,16 @@ cd /tmp && unzip impala_jdbc_2.5.45.1065.zip
 cd /tmp/ClouderaImpalaJDBC_2.5.45.1065
 unzip ClouderaImpalaJDBC41_2.5.45.zip
 
+cd $CURRENT_DIR
+
 # Create Kafka Topic
 kafka-topics --create --zookeeper $HOSTNAME:2181/kafka --topic cdfworkshoplogs --partitions 1 --replication-factor 1
 
+cd $CURRENT_DIR
+
 # Configure Solr Collection
 cp -r files/nifilogs.zip /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/
-cd /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/
-unzip nifilogs.zip
+cd /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/ && unzip nifilogs.zip
 cd /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/nifilogs/conf
 
 # Create Collection
