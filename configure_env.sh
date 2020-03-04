369c2cf5668e11332bcb63a325e06aeb00f7930c
root <root@ec2-34-205-39-171.compute-1.amazonaws.com>
2020-03-04 06:05:02 +0000

    added twitter

--- configure_env.sh
+++ configure_env.sh
@@ -21,12 +21,27 @@ kafka-topics --create --zookeeper $HOSTNAME:2181/kafka --topic cdfworkshoplogs -
 
 cd $CURRENT_DIR
 
-# Configure Solr Collection
+# Configure NIFI Solr Collection
 cp -r files/nifilogs.zip /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/
 cd /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/ && unzip nifilogs.zip
 cd /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/nifilogs/conf
 
-# Create Collection
+# Create NIFI Collection
 curl -X POST --header "Content-Type:application/octet-stream" --data-binary @nifilogs.zip "http://$HOSTNAME:8983/solr/admin/configs?action=UPLOAD&name=nifilogs"
 curl -X POST --header "Content-Type:application/octet-stream" "http://$HOSTNAME:8983/solr/admin/collections?action=CREATE&name=nifi&&collection.configName=nifilogs&&numShards=1"
+
+cd $CURRENT_DIR
+
+# Configure Tweets Solr Collection
+cp -r files/tweets.zip /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/
+cd /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/ && unzip tweets.zip
+cd /opt/cloudera/parcels/CDH-6.3.2-1.cdh6.3.2.p0.1605554/lib/solr/server/solr/configsets/tweets/conf
+
+# Create TWEETS Collection
+curl -X POST --header "Content-Type:application/octet-stream" --data-binary @tweets.zip "http://$HOSTNAME:8983/solr/admin/configs?action=UPLOAD&name=tweets"
+curl -X POST --header "Content-Type:application/octet-stream" "http://$HOSTNAME:8983/solr/admin/collections?action=CREATE&name=tweets&&collection.configName=tweets&&numShards=1"
+
+
+
+
 
